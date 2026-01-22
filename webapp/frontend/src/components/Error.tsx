cf8da3619425145656641d6a4ebbba97ff1bd50e
Jpoker01 <jiripokorny455@gmail.com>
2026-01-22 18:00:01 +0100

    Update Error.tsx

--- webapp/frontend/src/components/Error.tsx
+++ webapp/frontend/src/components/Error.tsx
@@ -8,7 +8,7 @@ export function Error({ error }: ErrorSectionProperties) {
   return (
 <div className="rounded-2xl shadow-lg p-4 mb-8 pb-2 animate-fadeIn bg-red-50">
   <div className="flex items-center gap-4 ml-2 mb-3">
-    <CircleX className="w-11 h-11 text-red-500 flex-shrink-0" />
+    <CircleX className="w-11 h-11 text-red-500 flex-shrink-0 mt-0.5" />
     <div className="flex-1">
       <h3 className="text-lg font-semibold pb-1.5 text-slate-700">Error</h3>
       <p className="text-md text-slate-800">{error}</p>
